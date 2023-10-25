545cb1e9d3a7d2e2e5b802fcd30b3e730ddfb96b
Ilya G <motoslam@mail.ru>
2023-10-25 18:32:54 +0300

    timezone for created_at

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Filterable;
+use Carbon\Carbon;
 
 class Document extends Model
 {
@@ -16,6 +17,13 @@ class Document extends Model
         'documentable_id',
         'documentable_type'
     ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::createFromTimestamp(strtotime($value))
+            ->timezone(config('app.timezone'))
+            ->toDateTimeString();
+    }
 
     public function documentable()
     {
